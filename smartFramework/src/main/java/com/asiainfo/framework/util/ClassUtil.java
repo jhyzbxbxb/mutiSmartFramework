1b8c6b5cbfd48a52364e23605dd1773c5e8d5469
739691499@qq.com <hz19891029>
2017-08-04 15:19:59 +0800

    电脑端修改

--- smartFramework/src/main/java/com/asiainfo/framework/util/ClassUtil.java
+++ smartFramework/src/main/java/com/asiainfo/framework/util/ClassUtil.java
@@ -3,6 +3,11 @@ package com.asiainfo.framework.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ClassUtil {
     private static final Logger LOGGER = LoggerFactory.getLogger(ClassUtil.class);
 
@@ -13,4 +18,49 @@ public class ClassUtil {
     public static ClassLoader getClassLoader(){
         return  Thread.currentThread().getContextClassLoader();
     }
+
+    /**
+     * 加载类
+     * @param className
+     * @param isInitialized
+     * @return
+     */
+    public static Class<?> loadClass(String className, boolean isInitialized){
+        Class<?> cls;
+        try {
+            cls = Class.forName(className,isInitialized,getClassLoader());
+        }catch (ClassNotFoundException e){
+            LOGGER.error("load class failure",e);
+            throw new RuntimeException(e);
+        }
+        return cls;
+    }
+
+    /**
+     * 获取指定包名下的类
+     * @param packageName
+     * @return
+     */
+    public static Set<Class<?>> getClassSet(String packageName ){
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        try {
+            Enumeration<URL> urls = getClassLoader().getResources(packageName.replace(".","/"));
+            while (urls.hasMoreElements()){
+                URL url = urls.nextElement();
+                if(url!=null){
+                    String proptocol = url.getProtocol();
+                }
+            }
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        return classSet;
+    }
+
+    public static void main(String[] args){
+        System.out.println("111");
+        getClassSet("com.asiainfo.framework");
+    }
+
+
 }
